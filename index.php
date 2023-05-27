7cb1cc14bb762805716269ca2949d2d91814cbd3
Juan Contreras <juan26_2000@live.com.mx>
2023-05-26 19:09:23 -0600

    actualizdo el index y terminda la pagina de mi perfil

--- index.php
+++ index.php
@@ -14,87 +14,80 @@ $result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <title>Prestamo de libros</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/estilo.css" rel="stylesheet">
-    </head>
-    <style>
-        
-        table {
-            border-collapse: collapse;
-            width: 100%;
-            font-family: Arial, Helvetica, sans-serif;
-        }
 
-        th, td {
-            text-align: left;
-            padding: 8px;
-        }
+<head>
+    <title>Prestamo de libros</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/estilo.css" rel="stylesheet">
+</head>
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+        font-family: Arial, Helvetica, sans-serif;
+    }
 
-        tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
+    th,
+    td {
+        text-align: left;
+        padding: 8px;
+    }
 
-        .button-container {
-            margin-top: 10px;
-        }
-        .book-image {
-            max-height: 150px;
-        }
-    </style>
-    <body>
-        <h1>Hola Internauta</h1>
-        <a href="nuevoUsuario.php">Registrarse</a>
-        <a href="loginForm.php">Login</a>
-        <br>
-        <!-- Inicio de zona para mostrar libros -->
-        <div>
-            <h1>Libros disponibles:</h1><form method="POST">
-            <form method="POST">
-                <label for="mostrar_solo_disponibles">Mostrar solo disponibles:</label>
-                <input type="checkbox" id="mostrar_solo_disponibles" name="mostrar_solo_disponibles" <?php echo $mostrarSoloDisponibles ? 'checked' : ''; ?>>
-                <input type="submit" value="Filtrar">
-            </form>
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
 
-            <table>
+    .button-container {
+        margin-top: 10px;
+    }
+
+    .book-image {
+        max-height: 150px;
+    }
+</style>
+
+<body>
+    <h1>Hola Internauta</h1>
+    <a href="nuevoUsuario.php">Registrarse</a>
+    <a href="loginForm.php">Login</a>
+    <br>
+    <!-- Inicio de zona para mostrar libros -->
+    <div>
+        <h1>Libros disponibles:</h1>
+        <form method="POST">
+            <label for="mostrar_solo_disponibles">Mostrar solo disponibles:</label>
+            <input type="checkbox" id="mostrar_solo_disponibles" name="mostrar_solo_disponibles" <?php echo $mostrarSoloDisponibles ? 'checked' : ''; ?>>
+            <input type="submit" value="Filtrar">
+        </form>
+
+        <table>
+            <tr>
+                <th>ISBN</th>
+                <th>Nombre</th>
+                <th>Autor</th>
+                <th>Descripción</th>
+                <th>Foto</th>
+            </tr>
+            <?php while ($row = $result->fetch_assoc()) : ?>
                 <tr>
-                    <th>ISBN</th>
-                    <th>Nombre</th>
-                    <th>Autor</th>
-                    <th>Descripción</th>
-                    <th>Foto</th>
-                    <th>Acción</th>
+                    <td><?php echo $row['ISBN']; ?></td>
+                    <td><?php echo $row['nombre']; ?></td>
+                    <td><?php echo $row['autor']; ?></td>
+                    <td><?php echo $row['descripcion']; ?></td>
+                    <td>
+                        <?php if (!empty($row['foto'])) : ?>
+                            <img src="uploads/<?php echo $row['foto']; ?>" alt="Foto del libro" class="book-image">
+                        <?php else : ?>
+                            No disponible
+                        <?php endif; ?>
+                    </td>
                 </tr>
-                <?php while($row = $result->fetch_assoc()): ?>
-                    <tr>
-                        <td><?php echo $row['ISBN']; ?></td>
-                        <td><?php echo $row['nombre']; ?></td>
-                        <td><?php echo $row['autor']; ?></td>
-                        <td><?php echo $row['descripcion']; ?></td>
-                        <td>
-                            <?php if (!empty($row['foto'])): ?>
-                                <img src="uploads/<?php echo $row['foto']; ?>" alt="Foto del libro" class="book-image">
-                            <?php else: ?>
-                                No disponible
-                            <?php endif; ?>
-                        </td>
-                        <td>
-                            <?php if ($row['estatus'] == 1): 
-                            echo  '<a href="solicitarlibro.php?idLibro=' . $row['idLibros'] . '">Solicitar</a>';    
-                            ?>
-                                
-                            <?php else: ?>
-                                <button type="button" class="button" disabled>No disponible</button>
-                            <?php endif; ?>
-                        </td>
-                    </tr>
-                <?php endwhile; ?>
-            </table>
+            <?php endwhile; ?>
+        </table>
 
-        </div>
-    </body>
+    </div>
+</body>
 
 
 </html>
